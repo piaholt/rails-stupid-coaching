dce324b3fe9dd8a834fa74c5e30b7526295ce6fc
Pia Holtappels <pia.holtappels@icloud.com>
2022-11-14 14:49:07 +0000

    rails new

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    if params[:question].to_s.include?("?")
+      @answer = 'Silly question, get dressed and go to work!'
+    elsif params[:question].to_s.include?('I am going to work')
+      @answer = 'Great!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
